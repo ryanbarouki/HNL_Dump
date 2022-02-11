a434096f8e1cc9ca3e0ff942cf2efcb9c79baa82
Ryan <ryanbarouki@gmail.com>
2022-02-11 13:48:30 +0000

    improve argument parsing

--- BEBC_main.py
+++ BEBC_main.py
@@ -1,28 +1,14 @@
 #!/usr/local/bin/python3
+import argparse
 import matplotlib.pyplot as plt
 from beam import BeamExperiment
 from mixing_type import MixingType
-import sys
 import time
 from constants import *
 from signal_processor import SignalProcessor
 
 def main():
-    if len(sys.argv) < 4:
-        raise Exception("Not enough arguments.\nPlease specify the HNL mass, the number of samples and the mixing type\n\
-                        Usage: python BEBC_main.py <HNL mass in GeV> <number samples> -e/-t (-p)")
-    hnl_mass = float(sys.argv[1])
-    num_samples = int(sys.argv[2]) 
-    plot = False
-    mixing_type = MixingType.tau
-    for flag in sys.argv[3:]:
-        if flag == "-e":
-            mixing_type = MixingType.electron
-        elif flag == "-t":
-            mixing_type == MixingType.tau
-
-    if "-p" in sys.argv:
-        plot = True
+    hnl_mass, num_samples, plot, mixing_type = parse_arguments()
 
     start = time.time()
 
@@ -37,13 +23,28 @@ def main():
     end = time.time()
     print(f"Time taken: {end-start} seconds")
 
-    # plot lepton pair histogram
     if plot:
-        energies = [signal.momentum.get_energy() for signal in cut_signal]
-        pT = [signal.momentum.get_transverse_momentum() for signal in cut_signal]
-        plt.hist2d(energies, pT, bins=100, range=[[0,250], [0, 2]])
-        plt.show()
+        plot_signal(cut_signal)
 
+def plot_signal(cut_signal):
+    energies = [signal.momentum.get_energy() for signal in cut_signal]
+    pT = [signal.momentum.get_transverse_momentum() for signal in cut_signal]
+    plt.hist2d(energies, pT, bins=100, range=[[0,250], [0, 2]])
+    plt.show()
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-m', type=float, help="Mass of the HNL in GeV", required=True)
+    parser.add_argument('-n', type=int, help="Number of samples", required=True)
+    parser.add_argument('--mixing', type=str, choices=['electron', 'tau'], help="valid values: 'electron' or 'tau'", required=True)
+    parser.add_argument('--plot', action='store_true', help="A boolean value whether to plot")
+    args = parser.parse_args()
+
+    hnl_mass = args.m
+    num_samples = args.n
+    plot = args.plot
+    mixing_type = MixingType[args.mixing]
+    return hnl_mass,num_samples,plot,mixing_type
 
 if __name__ == "__main__":
     main()
