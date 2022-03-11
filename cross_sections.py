6f687eb261d5afaca7e6a2a8450b5d5c3ea28a73
Ryan Barouki <ryanbarouki@gmail.com>
2022-03-11 13:34:54 +0000

    adjust cross sections to have correct ratios

--- cross_sections.py
+++ cross_sections.py
@@ -1,6 +1,6 @@
-"""Proton to D0 + D0bar"""
-P_TO_D0D0_X = 18.3 # +- 2.5 micro barn
 """Proton to D+ + D-"""
-P_TO_DPDM_X = 11.9 # +- 1.5 micro barn
+P_TO_DPDM_X = 1 # we only need the ratios of cross sections
+"""Proton to D0 + D0bar"""
+P_TO_D0D0_X = 2*P_TO_DPDM_X 
 """Proton to Ds + Dsbar"""
-P_TO_DSDS_X = 0.2*(P_TO_D0D0_X + P_TO_DPDM_X) 
+P_TO_DSDS_X = 0.2*(P_TO_D0D0_X + P_TO_DPDM_X)
