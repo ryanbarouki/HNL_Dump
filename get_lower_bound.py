0b4e38a0146965116a0a76974d20a1f5a22c3a87
Ryan <ryanbarouki@gmail.com>
2022-03-14 20:40:26 +0000

    binary chop through mixing to get lower bound

--- get_lower_bound.py
+++ get_lower_bound.py
@@ -20,13 +20,22 @@ def main():
     hnl_masses = np.linspace(0.2, 1.2, 10)
     mixings_squared = np.linspace(1e-3, 1e-2, 10)
 
-    for mixing in mixings_squared:
+    lower_mixing = 1e-3
+    upper_mixing = 1e-2
+    mixing = (upper_mixing + lower_mixing)/2
+    prev_mixing = lower_mixing
+    it = 0
+    while abs((prev_mixing - mixing)/mixing) > 0.0005 and it < 25: 
         less_decays_than_observed = total_decays_less_than_observed(1.5, mixing, 10000, MixingType.electron)
         if less_decays_than_observed:
-            print(f"Mixing: {mixing}, less decays than observed") 
+            upper_mixing = mixing
         else:
-            print(f"Mixing: {mixing}, more decays than observed") 
-        print("================================================")
+            lower_mixing = mixing
+        it += 1
+        prev_mixing = mixing
+        mixing = (upper_mixing + lower_mixing)/2
+        print(abs(prev_mixing - mixing)/mixing)
+    print("Lower bound: {:e}".format(mixing))    
 
 
     # is_mixing_too_small(1.5, 6.21e-3, 10000, MixingType.electron)
