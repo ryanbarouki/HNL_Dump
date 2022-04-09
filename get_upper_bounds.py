d1658e9b2c91bbf108fecbfee2aac582bcc3bfbe
Ryan <ryanbarouki@gmail.com>
2022-04-09 13:56:31 +0100

    add ability for mass ranges in main program

--- get_upper_bounds.py
+++ get_upper_bounds.py
@@ -1,29 +1,64 @@
-
+#!/usr/local/bin/python3
+import numpy as np
+import argparse
 import matplotlib.pyplot as plt
 from beam import BeamExperiment
 from mixing_type import MixingType
 import time
 from signal_processor import SignalProcessor
 from logger import Logger
-import numpy as np
 
 def main():
-    logger = Logger(debug=False)
+    hnl_masses, mass_range, num_samples, debug, mixing_type, plot = parse_arguments()
+    logger = Logger(debug=debug)
     upper_bounds = []
-    hnl_masses = np.linspace(0.4, 1.8, 18, endpoint=False)
+
+    if mass_range is not None:
+        lower_mass, upper_mass, num_points = mass_range
+        hnl_masses = np.linspace(lower_mass, upper_mass, int(num_points))
+
     for hnl_mass in hnl_masses:
         start = time.time()
-        beam = BeamExperiment(mixing_type=MixingType.electron, num_samples=10000)
+
+        beam = BeamExperiment(mixing_type=mixing_type, num_samples=num_samples)
+
         beam.start_dump(hnl_mass)
         upper_bound_squared = SignalProcessor(beam).get_upper_bound()
         upper_bounds.append(upper_bound_squared)
         print(f"mass: {hnl_mass}, bound: {upper_bound_squared}")
+
         end = time.time()
         logger.log(f"Time taken: {end-start} seconds")
-    
-    plt.plot(hnl_masses, upper_bounds)
-    plt.yscale('log')
+
+    if plot:
+        plt.plot(hnl_masses, upper_bounds)
+        plt.yscale('log')
+        plt.show()
+
+def plot_signal(cut_signal):
+    energies = [momentum.get_energy() for momentum in cut_signal]
+    pT = [momentum.get_transverse_momentum() for momentum in cut_signal]
+    plt.hist2d(energies, pT, bins=100, range=[[0,250], [0, 2]])
     plt.show()
-    
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('-m', type=float, nargs='+', help="Masses of the HNL in GeV")
+    group.add_argument('--mass-range', type=float, nargs='+', help="Mass range '<lower> <higher> <number_of_points>' of the HNL in GeV")
+    parser.add_argument('-n', type=int, help="Number of samples", required=True)
+    parser.add_argument('--mixing', type=str, choices=['electron', 'tau'], help="valid values: 'electron' or 'tau'", required=True)
+    parser.add_argument('--debug', action='store_true', help="Print debug logs")
+    parser.add_argument('--plot', action='store_true', help="Plot upper bounds")
+    args = parser.parse_args()
+
+    hnl_masses = args.m
+    mass_range = args.mass_range
+    num_samples = args.n
+    debug = args.debug
+    plot = args.plot
+    mixing_type = MixingType[args.mixing]
+    return hnl_masses,mass_range,num_samples,debug,mixing_type,plot
+
 if __name__ == "__main__":
     main()
