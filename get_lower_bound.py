c7a3eb5c7414c86f0679896afe5c2462e2a32774
Ryan Barouki <ryanbarouki@gmail.com>
2022-04-14 10:21:57 +0100

    add command line arguments to get_lower_bound.py

--- get_lower_bound.py
+++ get_lower_bound.py
@@ -1,6 +1,7 @@
 #!/usr/local/bin/python3
 import argparse
 import numpy as np
+import matplotlib.pyplot as plt
 from beam import BeamExperiment
 from mixing_type import MixingType
 from signal_processor import SignalProcessor
@@ -15,25 +16,57 @@ def total_decays_less_than_observed(hnl_mass, mixing_squared, num_samples, mixin
     return total_decays_less_than_observed
 
 def main():
-    lower_mixing = 1e-5
-    upper_mixing = 1
-    mixing = (upper_mixing + lower_mixing)/2
-    prev_mixing = lower_mixing
-    it = 0
-    while abs((prev_mixing - mixing)/mixing) > 0.0005 and it < 25: 
-        print(f"Mixing: {mixing}")
-        less_decays_than_observed = total_decays_less_than_observed(1.5, mixing, 10000, MixingType.electron)
-        if less_decays_than_observed:
-            upper_mixing = mixing
-        else:
-            lower_mixing = mixing
-        it += 1
-        prev_mixing = mixing
-        mixing = (upper_mixing + lower_mixing)/2
-        print(abs(prev_mixing - mixing)/mixing)
-    print("Lower bound: {:e}".format(mixing))    
+    hnl_masses, mass_range, num_samples, debug, mixing_type, plot = parse_arguments()
+    logger = Logger(debug=debug)
+    lower_bounds = []
 
+    if mass_range is not None:
+        lower_mass, upper_mass, num_points = mass_range
+        hnl_masses = np.linspace(lower_mass, upper_mass, int(num_points))
+
+    for hnl_mass in hnl_masses:
+        lower_mixing = 1e-5
+        upper_mixing = 1
+        mixing = (upper_mixing + lower_mixing)/2
+        prev_mixing = lower_mixing
+        it = 0
+        while abs((prev_mixing - mixing)/mixing) > 0.0005 and it < 25: 
+            logger.log(f"Mixing: {mixing}")
+            less_decays_than_observed = total_decays_less_than_observed(hnl_mass, mixing, num_samples, mixing_type)
+            if less_decays_than_observed:
+                upper_mixing = mixing
+            else:
+                lower_mixing = mixing
+            it += 1
+            prev_mixing = mixing
+            mixing = (upper_mixing + lower_mixing)/2
+            logger.log(f"difference in mixing: {abs(prev_mixing - mixing)/mixing}")
+        lower_bounds.append(mixing)
+        print("Mass: {}, Lower bound: {:e}".format(hnl_mass, mixing))    
+    
+    if plot:
+        plt.plot(hnl_masses, lower_bounds)
+        plt.yscale('log')
+        plt.show()
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('-m', type=float, nargs='+', help="Masses of the HNL in GeV")
+    group.add_argument('--mass-range', type=float, nargs='+', help="Mass range '<lower> <higher> <number_of_points>' of the HNL in GeV")
+    parser.add_argument('-n', type=int, help="Number of samples", required=True)
+    parser.add_argument('--mixing', type=str, choices=['electron', 'tau'], help="valid values: 'electron' or 'tau'", required=True)
+    parser.add_argument('--debug', action='store_true', help="Print debug logs")
+    parser.add_argument('--plot', action='store_true', help="Plot upper bounds")
+    args = parser.parse_args()
+
+    hnl_masses = args.m
+    mass_range = args.mass_range
+    num_samples = args.n
+    debug = args.debug
+    plot = args.plot
+    mixing_type = MixingType[args.mixing]
+    return hnl_masses,mass_range,num_samples,debug,mixing_type,plot
 
 if __name__ == "__main__":
-    logger = Logger(debug=False)
     main()
