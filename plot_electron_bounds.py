403626ac27942e3dadabe3c58e9a9e03ceb645d1
Ryan <ryanbarouki@gmail.com>
2022-05-07 13:01:02 +0100

    more existing data on electron mixing bound plot

--- plot_electron_bounds.py
+++ plot_electron_bounds.py
@@ -1,3 +1,4 @@
+import math
 import os
 from turtle import color
 import numpy as np
@@ -22,13 +23,18 @@ def main():
     upper_bounds = read_csv_file(f"upper_bound_data/upper_bounds_{mixing_type}.csv")
     # lower_bounds = read_csv_file(f"upper_bound_data/upper_bounds_{mixing_type}.csv")
     lower_bounds = read_csv_file(f"lower_bound_data/lower_bounds_{mixing_type}.csv")
-    bebc_data = read_csv_file("./digitised_data/BEBC_bounds_electron.csv")
+    bebc = np.array(read_csv_file("./digitised_data/BEBC_electron.csv"))
+    dune = np.array(read_csv_file("./digitised_data/DUNE_electron.csv"))
+    mathusla = np.array(read_csv_file("./digitised_data/MATHUSLA_electron.csv"))
+    na62 = np.array(read_csv_file("./digitised_data/NA62_electron.csv"))
+    ship = np.array(read_csv_file("./digitised_data/SHiP_electron.csv"))
 
-    lower_bounds.sort(key=lambda data: data[0])
+    lower_bounds.sort(key=lambda data: data[0], reverse=True)
     upper_bounds.sort(key=lambda data: data[0])
     lower_bounds = np.array(lower_bounds)
     upper_bounds = np.array(upper_bounds)
-    bebc_data = np.array(bebc_data)
+    bounds = np.append(upper_bounds, lower_bounds, axis=0)
+    print(bounds)
 
     # # ------------------------ Plotting --------------------------------
     plt.figure(0)
@@ -40,13 +46,24 @@ def main():
         y_label = r'$|U_{e}|^2$'
         
     plt.ylabel(y_label)
-    plt.plot(upper_bounds[:,0], upper_bounds[:,1], 'k')
-    plt.plot(lower_bounds[:,0], lower_bounds[:,1], 'k')
-    plt.plot(bebc_data[:,0], bebc_data[:,1])
+    plt.plot(*get_cols(bounds), 'k')
+    plt.plot(*get_cols(bebc))
+    plt.plot(*get_cols(dune))
+    plt.plot(*get_cols(mathusla))
+    plt.plot(*get_cols(na62))
+    plt.plot(*get_cols(ship))
     plt.yscale('log')
     dirname = os.path.dirname(os.path.abspath(__file__))
     plt.savefig(os.path.join(dirname, f"recent_graphs/mass_bound_plot[{mixing_type}].png"), dpi=250)
     plt.show()
+
+def connectpoints(x,y,p1,p2):
+    x1, x2 = x[p1], x[p2]
+    y1, y2 = y[p1], y[p2]
+    plt.plot([x1,x2],[y1,y2])
+
+def get_cols(array: np.ndarray):
+    return (array[:,0], array[:,1])
 
 def read_csv_file(filename):
     with open(filename) as f:
